d9c2db8dcad10698d464f180fb8360d2df477bd2
zaidpantoja <alberthzaid2022@hotmail.com>
2024-10-20 09:18:52 -0500

    US002:cors resolved

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import route from "./Routes/Routes.js";
+import cors from 'cors';
 
 
 
 const app = express();
+app.use(cors())
 app.use(json())
 app.use(route)
 
